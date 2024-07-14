258ccf5c7616d3b9f0d07d293772661bb499d92d
Janry S LP <janryseran069407@gmail.com>
2024-07-14 20:35:00 +0800

    added register

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(AuthController::class)->group(function(){
+    Route::get('/register', 'register')->name('register');
+});
